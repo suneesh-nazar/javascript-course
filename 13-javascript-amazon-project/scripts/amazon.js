08bc20cba465d12013a6b9ebba3a44c3dfc1b900
Suneesh Nazar <suneesh.n@gmail.com>
2025-01-13 22:59:33 +0530

    fix: update script paths to relative URLs and enhance quantity selector functionality

--- 13-javascript-amazon-project/scripts/amazon.js
+++ 13-javascript-amazon-project/scripts/amazon.js
@@ -30,7 +30,7 @@ products.forEach ((product) => {
     <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -65,7 +65,6 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
-    //console.log(productName);
 
     let matchingItem;
 
@@ -76,11 +75,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity++;
+      matchingItem.quantity += Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: Number(document.querySelector(`.js-quantity-selector-${productId}`).value)
       });
     }
 
